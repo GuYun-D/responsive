936f129f3c37496076ceb788f9ee4123705a17c2
solitary-cloud <1946306647@qq.com>
2022-07-16 12:40:21 +0800

    🎉 init(project): construction project
    configure routing and tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+      src="https://img0.baidu.com/it/u=4194817038,3308327225&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
+      alt="图片"
+    />
+
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">我发现了好东西</p>
+    </div>
+  </div>
+</template>
