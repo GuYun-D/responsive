e2baaf8f5f4891180e6167200cb96c203779e380
solitary-cloud <1946306647@qq.com>
2022-07-17 23:55:56 +0800

    feat(navigation): ✨ 完成silder滑块的逻辑

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
+      ref="ulRef"
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- 滑块 -->
+      <li
+        ref="siderRef"
+        :style="sliderStyle"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+      ></li>
       <!-- 按钮 -->
       <li
         class="fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white z-20 shadow-l-white"
@@ -10,9 +17,14 @@
         <m-svg-icon name="hamburger" class="w-1.5 h-1.5"></m-svg-icon>
       </li>
       <li
-        v-for="item in data"
+        :ref="setNavItemRef"
+        v-for="(item, index) in data"
         :key="item.id"
+        :class="{
+          ' text-zinc-100': currentCategoryIndex === index
+        }"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        @click="onNavItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -21,12 +33,58 @@
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity'
+import { useScroll } from '@vueuse/core'
+import { onBeforeUpdate, watch } from '@vue/runtime-core'
+
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+const currentCategoryIndex = ref(0)
+const ulRef = ref()
+let navItemRefs = []
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+const setNavItemRef = (navItem) => {
+  if (navItem) {
+    navItemRefs.push(navItem)
+  }
+}
+
+const onNavItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
+
+onBeforeUpdate(() => {
+  /**
+   * setNavItemRef 在数据源变化引起DOM变化之后就会执行 所以
+   * navItemRefs内部的数据会越来越多
+   * 数据变化后 => DOM更新之前 置空
+   */
+  navItemRefs = []
+})
+
+// 获取响应式的 scroll 滚动距离
+const { x: ulScrollLeft } = useScroll(ulRef)
+
+watch(
+  () => currentCategoryIndex.value,
+  (val) => {
+    const { width, left } = navItemRefs[val].getBoundingClientRect()
+    sliderStyle.value = {
+      // 横向滚动位置：ul的横向滚动位置 + 当前元素的left - ul.padding
+      transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+      width: width + 'px'
+    }
+  }
+)
 </script>
 
 <style lang="scss" scoped></style>
