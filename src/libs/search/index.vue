97a63f213c8c80d60a1f91063e9021089c76ca01
solitary-cloud <1946306647@qq.com>
2022-07-22 19:39:21 +0800

    ✨ feat(search): 完善search组件的功能

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    ref="containerRef"
     class="group relative p-0.5 rounded-xl border-white duration-500 hover:bgred100/40"
   >
     <div>
@@ -10,35 +11,109 @@
       ></m-svg-icon>
 
       <input
+        v-model="inputValue"
         placeholder="搜索"
         type="text"
         class="group-hover:border-zinc-300 group-hover:bg-white block text-sm duration-500 w-full h-[44px] pl-4 outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide font-semibold border border-zinc-100 focus:border-red-300"
+        @keyup.enter="onClickSearchHandler"
+        @focus="onFocusHandler"
+        @blur="onBlurHandler"
       />
       <m-svg-icon
+        v-show="inputValue"
+        @click="onClickClearIcon"
         name="input-delete"
-        class="h-1.5 w-1.5 duration-500 absolute top-[50%] right-9 translate-y-[-50%]"
+        class="h-1.5 w-1.5 duration-500 absolute top-[50%] right-9 translate-y-[-50%] cursor-pointer"
       ></m-svg-icon>
       <div
         class="group-hover:opacity-100 opacity-0 h-1.5 w-[1px] absolute top-[50%] right-[62px] translate-y-[-50%] duration-500 bg-zinc-200"
       ></div>
-      <m-button
-        class="absolute translate-y-[-50%] top-[50%] right-1 rounded-full"
-        icon="search"
-        icon-color="#fff"
-      ></m-button>
     </div>
+    <m-button
+      class="group-hover:opacity-100 opacity-0 duration-500 absolute translate-y-[-50%] top-[50%] right-1 rounded-full"
+      icon="search"
+      icon-color="#fff"
+      @click="onClickSearchHandler"
+    ></m-button>
     <!-- drapdown -->
     <transition name="slider">
       <div
+        v-if="!$slots.drapdown"
+        v-show="isFocus"
         class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl"
       >
-        <slot name="drapdown"></slot>
+        <slot name="dropdown"></slot>
       </div>
     </transition>
   </div>
 </template>
 
-<script setup></script>
+<script>
+const EMIT_UPDATE_MODELVALUE = 'update:moadelValue'
+const EVENT_SEARCH = 'search'
+const EMIT_CLEAR = 'clear'
+const EMIT_INPUT = 'input'
+const EMIT_FOCUS = 'focus'
+const EMIT_BLUR = 'blur'
+
+export default {
+  name: 'search-name'
+}
+</script>
+
+<script setup>
+import { ref, watch } from 'vue'
+import { useVModel, onClickOutside } from '@vueuse/core'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits([
+  EMIT_UPDATE_MODELVALUE,
+  EVENT_SEARCH,
+  EMIT_CLEAR,
+  EMIT_INPUT,
+  EMIT_FOCUS,
+  EMIT_BLUR
+])
+
+const inputValue = useVModel(props)
+const isFocus = ref(false)
+const containerRef = ref(null)
+
+const onClickClearIcon = () => {
+  inputValue.value = ''
+  emit(EMIT_CLEAR, '')
+}
+
+const onClickSearchHandler = () => {
+  emit(EVENT_SEARCH, inputValue.value)
+}
+
+const onFocusHandler = () => {
+  isFocus.value = true
+  emit(EMIT_FOCUS)
+}
+
+onClickOutside(containerRef, () => {
+  isFocus.value = false
+})
+
+const onBlurHandler = () => {
+  emit(EMIT_BLUR)
+}
+
+watch(
+  () => inputValue,
+  (val) => {
+    emit(EMIT_INPUT, val)
+  }
+)
+</script>
 
 <style lang="scss" scoped>
 .slider-enter-active,
--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class=" w-screen">
-    <m-search></m-search>
+  <div class="w-screen">
+    <m-search v-model="modelValue">
+      <template #dropdown>
+        <div>dropdown</div>
+      </template>
+    </m-search>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const modelValue = ref('')
+</script>
 
 <style lang="scss" scoped></style>
