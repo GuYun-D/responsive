687c453be650226c7ae004436ae236f7ead913f4
solitary-cloud <1946306647@qq.com>
2022-07-31 18:07:23 +0800

    🐞 fix(infinite): 修复首次渲染不完整时无法获取数据的bug

--- src/libs/infinite/index.vue
+++ src/libs/infinite/index.vue
@@ -5,7 +5,7 @@
     <div ref="scrollRef" class="h-6 py-4">
       <!-- 加载更多 -->
       <m-svg-icon
-      v-show="loading"
+        v-show="loading"
         class="w-4 h-4 mx-auto animate-spin"
         name="infinite-load"
       ></m-svg-icon>
@@ -18,8 +18,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useVModel, useIntersectionObserver } from '@vueuse/core'
+import { ref, watch } from 'vue'
+import { useVModel, useIntersectionObserver, set } from '@vueuse/core'
 
 const props = defineProps({
   modelValue: {
@@ -37,15 +37,23 @@ const emits = defineEmits(['onLoad', 'update:modelValue'])
 const loading = useVModel(props)
 
 const scrollRef = ref(null)
+const targetIsIntersecting = ref(false)
 
 useIntersectionObserver(scrollRef, ([{ isIntersecting }]) => {
-  // 如果 scrollRef 可见了，isIntersecting 就是true
-  // 视图可见 && loading = false && 数据尚未加载完成
-  if (isIntersecting && !loading.value && !props.isFinished) {
-    loading.value = true
-    emits('onLoad')
-  }
+  targetIsIntersecting.value = isIntersecting
+  emitLoad()
 })
-</script>
 
-<style lang="scss" scoped></style>
+const emitLoad = () => {
+  setTimeout(() => {
+    // 如果 scrollRef 可见了，isIntersecting 就是true
+    // 视图可见 && loading = false && 数据尚未加载完成
+    if (targetIsIntersecting && !loading.value && !props.isFinished) {
+      loading.value = true
+      emits('onLoad')
+    }
+  }, 500)
+}
+
+watch(() => loading.value, emitLoad)
+</script>
