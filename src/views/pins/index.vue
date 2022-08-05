3349580ef3d9ed637f63451b0988fa26f1d98823
solitary-cloud <1946306647@qq.com>
2022-08-05 22:41:11 +0800

    🐞 fix(pins): 修复pins刷新白屏的问题

---
+++ src/views/pins/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-full bg-zinc-200 dark:bg-zinc-800">
+    <Pins :id="$route.params.id"></Pins>
+  </div>
+</template>
+
+<script setup>
+import Pins from './components/pins.vue'
+</script>
+
+<style lang="scss" scoped>
+</style>
